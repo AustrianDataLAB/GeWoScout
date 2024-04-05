143f5523d463fee020303d5c664ff82959f58f52
Nikolaus Spring <nikolaus.spring@gmail.com>
2024-04-05 23:21:30 +0200

    Adjust for restful design

--- src/backend-api/GetListing/__init__.py
+++ src/backend-api/GetListing/__init__.py
@@ -5,25 +5,20 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest, existingDoc: func.DocumentList) -> func.HttpResponse:
+    pk = req.route_params.get('city')
     id = req.route_params.get('id')
-    logging.info('Retrieving listing %s', id)
+    logging.info('Retrieving listing %s : %s', pk, id)
 
     if not existingDoc or existingDoc.data == None or len(existingDoc.data) == 0:
-        return func.HttpResponse("Listing not found", status_code=404)
+        return func.HttpResponse(status_code=404)
     
     if len(existingDoc.data) > 1:
-        logging.warn("Found more than one listing with the same id %s", id)
+        logging.warning("Found more than one listing with the same id %s", id)
 
     data = existingDoc.data[0].to_dict()
 
     # Remove CosmosDB metadata
-    if "_rid" in data:
-        del data["_rid"]
-    if "_self" in data:
-        del data["_self"]
-    if "_ts":
-        del data["_ts"]
-    if "_etag":
-        del data["_etag"]
+    for key in ["_rid", "_self", "_ts", "_etag"]:
+        data.pop(key, None)
 
     return func.HttpResponse(json.dumps(data), status_code=200, mimetype="application/json")
