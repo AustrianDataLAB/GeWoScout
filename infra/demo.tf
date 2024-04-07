0d9780ce80434470fcbac98f9e27da600faed2d3
Laurenz K <45950275+laurenz-k@users.noreply.github.com>
2024-04-07 15:48:19 +0200

    Update demo.tf

--- infra/demo.tf
+++ infra/demo.tf
@@ -1,38 +1,71 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
+  }
   backend "azurerm" {
-    resource_group_name  = "<resourcegroupname>"
-    storage_account_name = "<storageaccount-name>"
-    container_name       = "<container-name"
-    key                  = "<Key Value>"
+    use_azuread_auth = true
+  }
+  required_version = ">= 0.13"
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+provider "azurerm" {
+  subscription_id   = var.subscription_id
+  tenant_id         = var.tenant_id
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
   }
 }
- 
-provider "azurerm" {
-  # The "feature" block is required for AzureRM provider > 2.x.
-  version = ">= 3.50.0"
-  features {}
+
+# TODO read the rg created by C for GeWoScout
+data "azurerm_resource_group" "rgruntime" {
+  name = "rg-service-gepip35"
 }
- 
-data "azurerm_client_config" "current" {}
- 
-#Create Resource Group
-resource "azurerm_resource_group" "rgsa" {
-  name     = "<resourcegroup1-name>"
-  location = "eastus2"
+
+// storage account contains the filesystem of our function
+resource "azurerm_storage_account" "storage" {
+  name                     = "gewoscoutdevstorage"
+  resource_group_name      = data.azurerm_resource_group.rgruntime.name
+  location                 = data.azurerm_resource_group.rgruntime.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
- 
-#Create Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "rgsa0102-vnet"
-  address_space       = ["<CIDR Range>"]
-  location            = "eastus2"
-  resource_group_name = azurerm_resource_group.rgsa.name
+
+// defines resources available to function: Y1 has free tier
+resource "azurerm_service_plan" "serviceplan" {
+  name                = "gewoscout-dev-serviceplan"
+  resource_group_name = data.azurerm_resource_group.rgruntime.name
+  location            = data.azurerm_resource_group.rgruntime.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
 }
- 
-# Create Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.rgsa.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "<CIDR Range>"
+
+// function
+resource "azurerm_linux_function_app" "function" {
+  name                = "gewoscout-dev-function-app"
+  resource_group_name = data.azurerm_resource_group.rgruntime.name
+  location            = data.azurerm_resource_group.rgruntime.location
+
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+  service_plan_id            = azurerm_service_plan.serviceplan.id
+
+  site_config {
+    application_stack {
+      python_version = "3.9"
+    }
+  }
 }
