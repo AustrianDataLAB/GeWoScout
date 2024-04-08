e0c6e1b1b2c7a86a93f4d2835dbeb3662acc2b63
brotholomew <bartoszek.blach@gmail.com>
2024-04-08 21:44:26 +0200

    corrected the terraform fmt command
    Signed-off-by: brotholomew <bartoszek.blach@gmail.com>

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
-  subscription_id   = var.infra_subscription_id
-  tenant_id         = var.infra_tenant_id
+  subscription_id = var.infra_subscription_id
+  tenant_id       = var.infra_tenant_id
   features {
     key_vault {
       purge_soft_delete_on_destroy    = true
