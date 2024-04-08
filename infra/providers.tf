7388abb90f84bfddf2ad6367b8aefdcaa90535d5
brotholomew <bartoszek.blach@gmail.com>
2024-04-08 21:48:32 +0200

    remove the duplicate configuration
    Signed-off-by: brotholomew <bartoszek.blach@gmail.com>

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,5 +1,12 @@
 provider "azurerm" {
-  features {}
+  subscription_id = var.infra_subscription_id
+  tenant_id       = var.infra_tenant_id
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
 }
 
 terraform {
