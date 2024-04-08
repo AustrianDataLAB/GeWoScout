fc12f3355d4f0e92bd504d06d12fe1e0b757a7f0
brotholomew <bartoszek.blach@gmail.com>
2024-04-08 21:49:54 +0200

    provided the container name
    Signed-off-by: brotholomew <bartoszek.blach@gmail.com>

--- infra/providers.tf
+++ infra/providers.tf
@@ -13,7 +13,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = var.state_resource_group_name
     storage_account_name = var.state_storage_account_name
-    container_name       = var.state_container_name
+    container_name       = "tfgewoscout"
     key                  = "terraform-base.tfstate"
   }
 }
