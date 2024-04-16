c26cb838cb7565de965bbfae7df04ed7ed8ba0bd
Nikolaus Spring <54682104+nspring00@users.noreply.github.com>
2024-04-17 00:15:25 +0200

    Optimize env handling

--- backend/cosmos/helpers.go
+++ backend/cosmos/helpers.go
@@ -11,37 +11,31 @@ import (
 // Helper function for setting up the db connection to a certain endpoint,
 // database and container.
 func GetContainer() (*azcosmos.ContainerClient, error) {
-	endpoint, ok := os.LookupEnv("DB_URI")
+	connectionString, ok := os.LookupEnv("COSMOS_DB_CONNECTION")
 	if !ok {
-		log.Fatal("DB_URI could not be found")
-		return nil, errors.New("DB_URI could not be found")
-	}
-
-	key, ok := os.LookupEnv("DB_PRIMARY_KEY")
-	if !ok {
-		log.Fatal("DB_PRIMARY_KEY could not be found")
-		return nil, errors.New("DB_PRIMARY_KEY could not be found")
+		log.Fatal("CosmosDbConnection could not be found")
+		return nil, errors.New("CosmosDbConnection could not be found")
 	}
 
 	dbName, ok := os.LookupEnv("DB_NAME")
 	if !ok {
-		log.Fatal("DB_NAME could not be found")
-		return nil, errors.New("DB_NAME could not be found")
+		log.Printf("Using default DB_NAME")
+		dbName = "gewoscout-db"
 	}
 
-	cred, err := azcosmos.NewKeyCredential(key)
-	if err != nil {
-		log.Fatal("Failed to create credentials from DB_PRIMARY_KEY")
-		return nil, err
+	containerName, ok := os.LookupEnv("DB_CONTAINER_NAME")
+	if !ok {
+		log.Printf("Using default DB_CONTAINER_NAME")
+		containerName = "ListingsByCity"
 	}
 
-	client, err := azcosmos.NewClientWithKey(endpoint, cred, nil)
+	client, err := azcosmos.NewClientFromConnectionString(connectionString, nil)
 	if err != nil {
 		log.Fatal("Failed to create client")
 		return nil, err
 	}
 
-	container, err := client.NewContainer(dbName, "ListingsByCity")
+	container, err := client.NewContainer(dbName, containerName)
 	if err != nil {
 		log.Fatal("Failed to get container Listings")
 		return nil, err
