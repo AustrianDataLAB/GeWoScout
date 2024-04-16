e1123a42767723ea483456e799ddb801a20c35eb
Michael Kurz <michael_kurz@posteo.at>
2024-04-16 15:18:33 +0200

    add frontend test pipeline

--- frontend/vue-gewoscout/vitest.config.ts
+++ frontend/vue-gewoscout/vitest.config.ts
@@ -6,6 +6,7 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      watch: false,
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url))
