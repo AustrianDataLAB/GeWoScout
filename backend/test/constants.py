d123b1816fe89f1ba5ac8be6f4b5300e6993b708
Nikolaus Spring <54682104+nspring00@users.noreply.github.com>
2024-04-17 20:10:04 +0200

    use localhost

--- backend/test/constants.py
+++ backend/test/constants.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-CONNECTION_STRING = "AccountEndpoint=https://127.0.0.1:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==;"
+CONNECTION_STRING = "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==;"
 
 API_BASE_URL = "http://localhost:8000/api/"
 
