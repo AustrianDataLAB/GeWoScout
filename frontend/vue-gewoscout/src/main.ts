806c9e066dd2f1f1bb1ff5d266ecc2018d759202
julia <julia.sme27@gmail.com>
2024-04-17 02:56:53 +0200

    add primevue, primeicon, primeflex

--- frontend/vue-gewoscout/src/main.ts
+++ frontend/vue-gewoscout/src/main.ts
@@ -1,6 +1,17 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
 
-createApp(App).mount('#app')
+import './assets/main.css'
+import 'primevue/resources/themes/lara-light-amber/theme.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css';
+
+
+const app = createApp(App);
+app.use(PrimeVue);
+
+app.component('vueButton', Button);
+
+app.mount('#app');
