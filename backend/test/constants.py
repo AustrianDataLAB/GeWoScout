eadcd435fbc55c078d73c65151676bfee7965f99
Nikolaus Spring <54682104+nspring00@users.noreply.github.com>
2024-04-17 16:33:03 +0200

    Make test individually runnable again

--- backend/test/constants.py
+++ backend/test/constants.py
@@ -1,8 +1,11 @@
 import json
+import os
 
 CONNECTION_STRING = "<your_connection_string>"
 
 API_BASE_URL = "http://localhost:8000/api/"
 
-with open('listings_fixture.json', 'r') as file:
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+with open(os.path.join(DIR_PATH, 'listings_fixture.json'), 'r') as file:
     LISTINGS_FIXTURE = json.load(file)
