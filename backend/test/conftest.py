31c8e9b852beacd56c1d8089e156acf61fb32884
Nikolaus Spring <54682104+nspring00@users.noreply.github.com>
2024-04-18 18:46:07 +0200

    Add working CI for integration tests (#4)
    * Start working on backend integration CI
    
    * Import emulator cert
    
    * run every push
    
    * update ci
    
    * ci update
    
    * use windows
    
    * Use newer buildx version
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * use localhost
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * Add script to prepare db and container and only delete items not db for cleanup
    
    * call fixture prep
    
    * Fix import
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * Revert unintentially commited changes
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * put func and test into same block
    
    * test docker directly
    
    * ci
    
    * try to fix dockerfile
    
    * Add health check
    
    * revert dockerfile and try to use linux containers
    
    * Fix getListings response format
    
    * Automatically start up func server on testing
    
    * ci
    
    * Hopefully fix func server startup
    
    * Cleanup ci file
    
    * Upload results
    
    * ci
    
    * update setup versions
    
    * improve ci
    
    * Add more cache
    
    * c
    
    * ci
    
    * Try to fix report upload
    
    * Use newer emulator action
    
    * make results nicer

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -1,7 +1,15 @@
+import os
+import subprocess
+import time
+
 import pytest
+import requests
+import urllib3
 from azure.cosmos import PartitionKey, CosmosClient, exceptions
 
 from .constants import CONNECTION_STRING, LISTINGS_FIXTURE
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 @pytest.fixture(scope="module")
@@ -37,4 +45,51 @@ def cosmos_db_setup(cosmos_db_client):
     yield database, container
 
     # Cleanup: delete database
-    cosmos_db_client.delete_database(database_name)
+    # cosmos_db_client.delete_database(database_name)
+
+    # Cleanup: remove all items from the container
+    for item in container.read_all_items():
+        container.delete_item(item['id'], partition_key=item['_partitionKey'])
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_backend_server():
+    # Start the server in the background
+    print("Starting the server...")
+
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    parent_dir = os.path.dirname(file_dir)
+    print("Working directory: ", parent_dir)
+
+    proc = subprocess.Popen(["func", "start", "--port", "8000"], shell=True, cwd=parent_dir)
+
+    try:
+        # Wait for the server to be up by checking the health endpoint
+        timeout = 60
+        start_time = time.time()
+        url = "http://localhost:8000/api/health"
+
+        while True:
+            try:
+                response = requests.get(url)
+                if response.status_code == 200:
+                    print("Server is up and running!")
+                    break
+            except requests.ConnectionError:
+                pass  # The server is not up yet
+
+            if time.time() - start_time > timeout:
+                raise TimeoutError("Timed out waiting for the server to start")
+            time.sleep(1)  # Wait a bit before trying again
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        proc.kill()  # Ensure server is killed if setup fails
+        raise
+
+    yield
+
+    # Teardown: stop the server
+    print("Stopping the server...")
+    proc.terminate()
+    proc.wait()
