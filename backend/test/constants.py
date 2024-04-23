9932e97fc6002b35886ffbd5a9a89af0d1f09616
Michael Koeppl <michael@koeppl.dev>
2024-04-23 02:19:14 +0200

    Allow overwriting env variables for tests

--- backend/test/constants.py
+++ backend/test/constants.py
@@ -1,9 +1,9 @@
 import json
 import os
 
-CONNECTION_STRING = "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==;"
+CONNECTION_STRING = os.getenv("CONNECTION_STRING", default="AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==;")
 
-API_BASE_URL = "http://localhost:8000/api/"
+API_BASE_URL = os.getenv("API_BASE_URL", default="http://localhost:8000/api/")
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
