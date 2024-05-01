e56b42096b28e7ae66b05e84637273c003d41e6d
brotholomew <bartoszek.blach@gmail.com>
2024-05-01 16:59:35 +0200

    update storage account container name
    Signed-off-by: brotholomew <bartoszek.blach@gmail.com>

--- infra/providers.tf
+++ infra/providers.tf
@@ -13,7 +13,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = var.state_resource_group_name
     storage_account_name = var.state_storage_account_name
-    container_name       = "tfgewoscout"
+    container_name       = "terraform"
     key                  = "terraform-base.tfstate"
   }
 }
