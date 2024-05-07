5d4f8e7a93c2dc1851f1b7fcee6b118bc53c1a29
Nikolaus Spring <54682104+nspring00@users.noreply.github.com>
2024-05-07 23:58:47 +0200

    Implement cosmos GetNonExistentIds helper

--- backend/cosmos/helpers.go
+++ backend/cosmos/helpers.go
@@ -1,7 +1,12 @@
 package cosmos
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"slices"
 	"strings"
+	"time"
 
 	"github.com/AustrianDataLAB/GeWoScout/backend/models"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
@@ -42,4 +47,82 @@ func GetQueryItemsPager(container *azcosmos.ContainerClient, city string, query 
 	}
 
 	return container.NewQueryItemsPager(sb.String(), partitionKey, &options)
+}
+
+func GetNonExistingIds(ctx context.Context, container *azcosmos.ContainerClient, idsByPk map[string][]string) (map[string][]string, error) {
+	nonExistingIds := make(map[string][]string)
+
+	for pk, ids := range idsByPk {
+		nonExistingIds[pk] = []string{}
+
+		existingIds, err := GetExistingIds(ctx, container, map[string][]string{pk: ids})
+		if err != nil {
+			return nil, err
+		}
+
+		for _, id := range ids {
+			if !slices.Contains(existingIds[pk], id) {
+				nonExistingIds[pk] = append(nonExistingIds[pk], id)
+			}
+		}
+	}
+
+	return nonExistingIds, nil
+}
+
+func GetExistingIds(ctx context.Context, container *azcosmos.ContainerClient, idsByPk map[string][]string) (map[string][]string, error) {
+	existingIds := make(map[string][]string)
+
+	for pk, ids := range idsByPk {
+
+		existingIds[pk] = []string{}
+
+		partitionKey := azcosmos.NewPartitionKeyString(pk)
+		placeholder, parameters := genStringParamList(ids)
+
+		query := fmt.Sprintf("SELECT c.id FROM c WHERE c.id IN (%s)", strings.Join(placeholder, ", "))
+
+		pager := container.NewQueryItemsPager(query, partitionKey, &azcosmos.QueryOptions{
+			QueryParameters: parameters,
+		})
+
+		for pager.More() {
+			ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+			page, err := pager.NextPage(ctx)
+			cancel()
+			if err != nil {
+				return nil, fmt.Errorf("failed to get next page: %w", err)
+			}
+
+			type IdItem struct {
+				ID string `json:"id"`
+			}
+
+			for _, item := range page.Items {
+				var idItem IdItem
+				err := json.Unmarshal(item, &idItem)
+				if err != nil {
+					return nil, fmt.Errorf("failed to unmarshal item: %w", err)
+				}
+
+				existingIds[pk] = append(existingIds[pk], idItem.ID)
+			}
+		}
+	}
+
+	return existingIds, nil
+}
+
+func genStringParamList(ids []string) ([]string, []azcosmos.QueryParameter) {
+	placeholders := make([]string, len(ids))
+	parameters := make([]azcosmos.QueryParameter, len(ids))
+	for i, id := range ids {
+		placeholder := fmt.Sprintf("@id%d", i)
+		placeholders[i] = placeholder
+		parameters[i] = azcosmos.QueryParameter{
+			Name:  placeholder,
+			Value: id,
+		}
+	}
+	return placeholders, parameters
 }
