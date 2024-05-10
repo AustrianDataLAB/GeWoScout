bc648797405a56f3a63d57d9f00a17d574b87db2
Nikolaus Spring <nikolaus.spring@gmail.com>
2024-05-11 01:05:39 +0200

    Use http to connect to azurite

--- backend/test/constants.py
+++ backend/test/constants.py
@@ -2,7 +2,7 @@ import json
 import os
 
 COSMOS_CONNECTION_STRING = os.getenv("CONNECTION_STRING", default="AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==;")
-QUEUE_STORAGE_CONNECTION = os.getenv("QUEUE_STORAGE_CONNECTION", default="DefaultEndpointsProtocol=https;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=https://127.0.0.1:10001/devstoreaccount1;")
+QUEUE_STORAGE_CONNECTION = os.getenv("QUEUE_STORAGE_CONNECTION", default="DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;")
 
 API_BASE_URL = os.getenv("API_BASE_URL", default="http://localhost:8000/api")
 
