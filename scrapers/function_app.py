ac6da87f86d59716a99f0efd6e53afda5ace82e2
Laurenz Kirchmayr <kirchmayr.laurenz@gmail.com>
2024-05-13 21:48:25 +0200

    adds scraper deployment

--- scrapers/function_app.py
+++ scrapers/function_app.py
@@ -4,4 +4,7 @@ app = func.FunctionApp()
 
 # NOTE (Laurenz): Register the created scrapers in the function app
 from impl import scraper_bwsg
+from impl import scraper_demo
+
 app.register_functions(scraper_bwsg.bp)
+app.register_functions(scraper_demo.bp)
