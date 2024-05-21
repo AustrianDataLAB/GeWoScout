4e8b58e1263fd8a7ef130931651a55d8a0f2fd40
Guilherme Oliveira <e12243299@student.tuwien.ac.at>
2024-05-22 01:58:49 +0200

    Scraper for bwsg

--- scrapers/impl/scraper_bwsg.py
+++ scrapers/impl/scraper_bwsg.py
@@ -1,13 +1,15 @@
 import azure.functions as func
 from bs4 import BeautifulSoup
+from datetime import datetime, timezone
 import requests
 import logging
+import re
 import json
 import os
 
 URL = "https://immobilien.bwsg.at"
 PARAMS = {
-    'f[all][marketing_type]': 'rent', # Miete
+    # 'f[all][marketing_type]': 'rent', # Miete
 	'f[all][realty_type][0]': '2',    # Wohnung
     'f[all][realty_type][1]': '3',    # Haus
     'f[all][city]'          : 'Wien',
@@ -49,42 +51,168 @@ def bwsg_scraper(timerObj: func.TimerRequest, q: func.Out[str]) -> None:
         cur = pages.find(class_ = 'active')
         pages = pages.find_all('li')    
 
+    payload = dict()
+    payload["scraperId"] = 'bwsg_scraper_rent'
+
+    
+    payload["timestamp"] = datetime.now(timezone.utc).isoformat()
     Wohnungen = list()
 
-    count = 0
 
     for link in links:        
-        req = requests.request(method = 'GET', url = URL + links[0])
+        req = requests.request(method = 'GET', url = URL + link)
         soup = BeautifulSoup(req.text, 'html.parser')
         info = soup.find(class_ = 'container-wrapper')
+        
+        wohnung = dict()        
+        headline = soup.find(class_ = 'realty-detail-headline')
+        wohnung["title"] = headline.find('h1').get_text().strip()
+        address = headline.find(class_ = 'address').get_text().strip()
+        postal_code = address.split('\n')[0]
+        postal_code = re.findall(r'\d+', postal_code)
+        address = ''.join(address.split('\n')[1:])
+        address = ' '.join(address.split())
+        wohnung["address"] = address.strip()
+        wohnung["postalCode"] = postal_code[0]
+        wohnung["housingCooperative"] = "BWS-Gruppe"
+        wohnung["country"] = "Austria"
+        wohnung["city"] = "Vienna"
+        
         detail_infos  = info.find(class_ = 'realty-detail-info').find_all('li')
         
-        wohnung = dict()
+        items = dict()
         for detail in detail_infos:
             desc = detail.find(class_ = 'list-item-desc').get_text().strip()
             value = detail.find(class_ = 'list-item-value').get_text().strip()
-            wohnung[desc] = value
+            items[desc] = value
         
-        detail_preis  = info.find(class_ = 'rent-price-table w-100').find_all('tr')
+        Objektnr = items.get("Objektnr.", "").split("/")
         
-        for row in detail_preis:
+        if len(Objektnr) > 1:
+            wohnung["projectId"] = Objektnr[0]
+            wohnung["listingId"] = Objektnr[1]
+        else:
+            wohnung["projectId"] = Objektnr[0]
+            wohnung["listingId"] = ""
+            
+        roomCount = items.get("Zimmer", None)
+        wohnung["roomCount"] = int(roomCount) if roomCount is not None else None
+        square_meters = re.findall(r'(\d+[\.\,\d]*)', items["Wohnfläche"])
+        wohnung["squareMeters"] = float(square_meters[0].replace(',', '.'))
+        wohnung["availabilityDate"] = items.get("Beziehbar", "")
+        wohnung["yearBuilt"] = items.get("Baujahr", "")
+        
+        hwb = re.findall(r'(\d+[\.\,\d]*)', items.get("HWB", ""))
+        # Pay attention here, the HWB uses a dot as a decimal separator
+        hwb = float(hwb[0].replace(',', '.')) if len(hwb) > 0 else None
+        
+        hwb_class = re.findall(r'\b[A-G]{1}\b\+*', items.get("HWB", ""))
+        hwb_class = hwb_class[0] if len(hwb_class) > 0 else None
+        
+        wohnung["hwgEnergyClass"] = hwb_class
+        wohnung["hwgEnergy"] = hwb
+        
+        fgee = re.findall(r'(\d+[\.\,\d]*)', items.get("fGEE", ""))
+        fgee = float(fgee[0].replace(',', '.')) if len(fgee) > 0 else None
+        
+        fgee_class = re.findall(r'\b[A-G]{1}\b\+*', items.get("fGEE", ""))
+        fgee_class = fgee_class[0] if len(fgee_class) > 0 else None
+        
+        wohnung["fgeeEnergyClass"] = fgee_class
+        wohnung["fgeeEnergy"] = fgee
+        
+        detail_preis = info.find(class_ = 'realty-detail-prices')
+
+        rent_preis  = detail_preis.find(class_ = 'rent-price-table').find_all('tr')
+        
+        rent = dict()
+        for row in rent_preis:
             cols = row.find_all('td')
             desc = cols[0].get_text().strip()
             value = cols[1].get_text().strip()
-            wohnung[desc] = value
+            rent[desc] = value
             
-        extra_info = info.find(class_ = 'list-unstyled').find_all('li')
-        for detail in detail_infos:
-            desc = detail.find(class_ = 'list-item-desc').get_text().strip()
-            value = detail.find(class_ = 'list-item-value').get_text().strip()
-            wohnung[desc] = value
+        miete = rent.get("Miete:", "")
+        miete = re.findall(r'(\d+[\.\,\d]*)', miete)
+        miete = float(miete[0].replace('.', '')\
+            .replace(',', '.')) if len(miete) > 0 else None
         
-        explanation = info.find(class_ = 'costs-explanation').get_text().strip()
+        wohnung["rentPricePerMonth"] = miete
+        
+        kaufpreis = rent.get("Kaufpreis:", "")
+        kaufpreis = re.findall(r'(\d+[\.\,\d]*)', kaufpreis)
+        kaufpreis = float(kaufpreis[0].replace('.', '')\
+            .replace(',', '.')) if len(kaufpreis) > 0 else None
+        
+        wohnung["salePrice"] = kaufpreis
+        
+        add_rent = rent.get("monatliche Gesamtbelastung:", "")
+        add_rent = re.findall(r'(\d+[\.\,\d]*)', add_rent)
+        add_rent = float(add_rent[0].replace('.', '')\
+            .replace(',', '.')) if len(add_rent) > 0 else None
+        
+        if miete is not None:
+            wohnung["additionalFees"] = add_rent - miete
+        else:
+            wohnung["additionalFees"] = add_rent
+            
+        extras = detail_preis.find(class_ = 'list-unstyled').find_all('li')
+        
+        extras_cost = dict()
+        for extra in extras:
+            desc = extra.find(class_ = 'list-item-desc').get_text().strip()
+            value = extra.find(class_ = 'list-item-value').get_text().strip()
+            extras_cost[desc] = value
+        
+        deposit = extras_cost.get("Kaution:", "")
+        deposit = re.findall(r'(\d+[\.\,\d]*)', deposit)
+        deposit = float(deposit[0].replace('.', '')\
+            .replace(',', '.')) if len(deposit) > 0 else None
+        
+        wohnung["deposit"] = deposit
+        
+        fin_contr = extras_cost.get("Finanzierungsbeitrag:", "")
+        fin_contr = re.findall(r'\d+\,*\.*\d*\,*\d*', fin_contr)
+        fin_contr = float(fin_contr[0].replace('.', '')\
+            .replace(',', '.')) if len(fin_contr) > 0 else None
+        
+        wohnung["financialContribution"] = fin_contr
+        
+        coop_share = extras_cost.get("Provision:", "")
+        coop_share = re.findall(r'\d+\,*\.*\d*\,*\d*', coop_share)
+        coop_share = float(coop_share[0].replace('.', '')\
+            .replace(',', '.')) if len(coop_share) > 0 else None
+        
+        wohnung["cooperativeShare"] = coop_share
+        
+        if miete is not None and kaufpreis is not None:
+            wohnung["listingType"] = "Both"
+        elif kaufpreis is not None:
+            wohnung["listingType"] = "Sale"
+        else:
+            wohnung["listingType"] = "Rent"
+            
+        wohnung["detailsUrl"] = URL + link
+        
+        images = soup.find_all(class_ = 'carousel-inner')
+        
+        
+        # Returning the first image
+        image = images[0].find_all('img')[0].get('src')
+        
+        wohnung["previewImageUrl"] = image
+        
+        
+        explanation = info.find(class_ = 'costs-explanation')
+        
+        if explanation is not None:
+            explanation = explanation.get_text().strip()
+        
+        
         wohnung['cost-explanation'] = explanation
         
-        wohnung['link'] = URL + links[0]
-        wohnung['Unternehmen'] = 'BWSG'
-        count += 1
         Wohnungen.append(wohnung)
+    
+    payload["listings"] = Wohnungen
     
     q.set(json.dumps(Wohnungen).encode(encoding='UTF-8'))
