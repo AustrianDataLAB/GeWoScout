c40ce6141a1a2dd0848f410610cf3221ef52ff02
Michael Kurz <michael_kurz@posteo.at>
2024-05-21 19:06:18 +0200

    update vitest and add adjust wrapper in test to avoid config null error

--- frontend/vue-gewoscout/src/components/__tests__/SearchForm.spec.ts
+++ frontend/vue-gewoscout/src/components/__tests__/SearchForm.spec.ts
@@ -2,12 +2,27 @@ import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import SearchForm from '../SearchForm.vue'
+import PrimeVue from 'primevue/config'
+
+
 
 describe('SearchForm', () => {
   it('renders properly', () => {
     expect(SearchForm).toBeTruthy();
 
-    const wrapper = mount(SearchForm);
+    const wrapper = mount(SearchForm, {
+      global: {
+        plugins: [PrimeVue],
+        mocks: {
+          $primevue: {
+            config: {
+              ripple: true,
+            },
+          },
+        },
+      },
+    });
+
     expect(wrapper.text()).toContain('Type of acquisition');
     expect(wrapper.text()).toContain('Price €');
   })
