d9b4d1fb0cc0a8a38ccd923527d6ffa4ef1e8ff8
Michael Kurz <michael_kurz@posteo.at>
2024-05-21 20:43:20 +0200

    add development dummy api, run npm format

--- frontend/vue-gewoscout/src/components/SearchForm.vue
+++ frontend/vue-gewoscout/src/components/SearchForm.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-
 import type SearchInputs from '@/types/SearchInputs';
 import { ref } from 'vue';
 
@@ -18,19 +17,19 @@ const selectedAreaFrom = ref();
 const selectedAreaTo = ref();
 
 const searchInputs: SearchInputs = {
-  city: '',
-}
+  city: ''
+};
 
 const selectedCity = ref('vienna');
 const cities = ref([
-    { name: 'Vienna', code: 'vienna' },
-    { name: 'Graz', code: 'graz' }
+  { name: 'Vienna', code: 'vienna' },
+  { name: 'Graz', code: 'graz' }
 ]);
 
 const selectedGeno = ref();
 const genos = ref([
-    { name: 'bwsg', code: 'bwsg' },
-    { name: 'ÖVW', code: 'oevw' }
+  { name: 'bwsg', code: 'bwsg' },
+  { name: 'ÖVW', code: 'oevw' }
 ]);
 
 const selectedTypes = ref(['All']);
@@ -42,38 +41,93 @@ const types = ref(['All', 'Rent', 'Rent + Option to buy']);
     <div class="formgrid grid">
       <div class="field col-4">
         <label for="type">Type of acquisition</label>
-        <SelectButton id="type" v-model="selectedTypes" :options="types" multiple aria-labelledby="multiple" class="w-full" />
+        <SelectButton
+          id="type"
+          v-model="selectedTypes"
+          :options="types"
+          multiple
+          aria-labelledby="multiple"
+          class="w-full"
+        />
       </div>
       <div class="field col-4">
         <label for="city">City</label>
-        <Dropdown id="city" v-model="selectedCity" :options="cities" showClear optionLabel="name" optionValue="code" placeholder="Select a City" class="w-full" />
+        <Dropdown
+          id="city"
+          v-model="selectedCity"
+          :options="cities"
+          showClear
+          optionLabel="name"
+          optionValue="code"
+          placeholder="Select a City"
+          class="w-full"
+        />
       </div>
       <div class="field col-4">
         <label for="geno">Genossenschaft</label>
-        <Dropdown id="geno" v-model="selectedGeno" :options="genos" showClear optionLabel="name" optionValue="code" placeholder="Select a Genossenschaft" class="w-full" />
+        <Dropdown
+          id="geno"
+          v-model="selectedGeno"
+          :options="genos"
+          showClear
+          optionLabel="name"
+          optionValue="code"
+          placeholder="Select a Genossenschaft"
+          class="w-full"
+        />
       </div>
       <div class="field col-4">
         <label for="priceFrom">Price €</label>
         <div class="flex flex-row gap-2">
-          <InputNumber inputId="priceFrom" v-model="selectedPriceFrom" placeholder="from" inputClass="w-10rem" />
+          <InputNumber
+            inputId="priceFrom"
+            v-model="selectedPriceFrom"
+            placeholder="from"
+            inputClass="w-10rem"
+          />
           <p>-</p>
-          <InputNumber inputId="priceTo" v-model="selectedPriceTo" placeholder="to" inputClass="w-10rem" />
+          <InputNumber
+            inputId="priceTo"
+            v-model="selectedPriceTo"
+            placeholder="to"
+            inputClass="w-10rem"
+          />
         </div>
       </div>
       <div class="field col-2">
         <label for="roomsFrom">Rooms</label>
         <div class="flex flex-row gap-2">
-          <InputNumber inputId="roomsFrom" v-model="selectedRoomsFrom" placeholder="from" inputClass="w-full" />
+          <InputNumber
+            inputId="roomsFrom"
+            v-model="selectedRoomsFrom"
+            placeholder="from"
+            inputClass="w-full"
+          />
           <p>-</p>
-          <InputNumber inputId="roomsTo" v-model="selectedRoomsTo" placeholder="to" inputClass="w-full" />
+          <InputNumber
+            inputId="roomsTo"
+            v-model="selectedRoomsTo"
+            placeholder="to"
+            inputClass="w-full"
+          />
         </div>
       </div>
       <div class="field col-2">
         <label for="areaFrom">Area m²</label>
         <div class="flex flex-row gap-2">
-          <InputNumber inputId="areaFrom" v-model="selectedAreaFrom" placeholder="from" inputClass="w-full" />
+          <InputNumber
+            inputId="areaFrom"
+            v-model="selectedAreaFrom"
+            placeholder="from"
+            inputClass="w-full"
+          />
           <p>-</p>
-          <InputNumber inputId="areaTo" v-model="selectedAreaTo" placeholder="to" inputClass="w-full" />
+          <InputNumber
+            inputId="areaTo"
+            v-model="selectedAreaTo"
+            placeholder="to"
+            inputClass="w-full"
+          />
         </div>
       </div>
       <div class="field col-4 text-right align-self-end">
@@ -83,7 +137,4 @@ const types = ref(['All', 'Rent', 'Rent + Option to buy']);
   </div>
 </template>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
--- frontend/vue-gewoscout/src/components/__tests__/SearchResultList.spec.ts
+++ frontend/vue-gewoscout/src/components/__tests__/SearchResultList.spec.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from 'vitest';
 
-import { mount } from '@vue/test-utils'
-import SearchResultList from '../SearchResultList.vue'
+import { mount } from '@vue/test-utils';
+import SearchResultList from '../SearchResultList.vue';
 
 describe('SearchResultList', () => {
   it('renders properly', () => {
@@ -10,5 +10,5 @@ describe('SearchResultList', () => {
     const wrapper = mount(SearchResultList);
     // expect(wrapper.text()).toContain('Modern 3-Bedroom Apartment in Central Vienna');
     // expect(wrapper.text()).toContain('Cozy Studio in the Heart of the City');
-  })
-})
+  });
+});
