3e4462ffb13e23d0f7222d6acc3bab3945c94811
Laurenz Kirchmayr <kirchmayr.laurenz@gmail.com>
2024-06-15 14:29:23 +0200

    fixes name

--- infra/monitoring.tf
+++ infra/monitoring.tf
@@ -7,12 +7,11 @@ resource "azurerm_application_insights" "ai" {
 }
 
 resource "azurerm_portal_dashboard" "main-dashboard" {
-  name                = "GewoScout Main Dashboard"
+  name                = "GewoScout-Main-Dashboard"
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
 
   dashboard_properties = templatefile("../dashboards/main.tpl", {
-    dashboard_name  = "GewoScout Main Dashboard"
     subscription_id = "e31c37ff-9b82-4f6b-8337-51314cc300ff"
     resource_group  = data.azurerm_resource_group.rg.name
   })
