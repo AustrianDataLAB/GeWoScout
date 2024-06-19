84c9ca1f93d6c8892a912177ff83799f77ddbcd5
julia <julia.sme27@gmail.com>
2024-06-19 03:23:53 +0200

    try fix test

--- frontend/vue-gewoscout/src/components/__tests__/SearchForm.spec.ts
+++ frontend/vue-gewoscout/src/components/__tests__/SearchForm.spec.ts
@@ -5,6 +5,12 @@ import SearchForm from '../SearchForm.vue';
 import PrimeVue from 'primevue/config';
 import { createPinia, setActivePinia } from 'pinia';
 
+// Import PrimeVue and its styles
+import './assets/main.css';
+import 'primevue/resources/themes/lara-light-amber/theme.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
 describe('SearchForm', () => {
   beforeEach(() => {
     setActivePinia(createPinia());
@@ -19,7 +25,51 @@ describe('SearchForm', () => {
         mocks: {
           $primevue: {
             config: {
-              ripple: true
+              ripple: true,
+              locale: {
+                firstDayOfWeek: 0, // Setting Sunday as the first day of the week
+                dayNames: [
+                  'Sunday',
+                  'Monday',
+                  'Tuesday',
+                  'Wednesday',
+                  'Thursday',
+                  'Friday',
+                  'Saturday'
+                ],
+                dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                dayNamesMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+                monthNames: [
+                  'January',
+                  'February',
+                  'March',
+                  'April',
+                  'May',
+                  'June',
+                  'July',
+                  'August',
+                  'September',
+                  'October',
+                  'November',
+                  'December'
+                ],
+                monthNamesShort: [
+                  'Jan',
+                  'Feb',
+                  'Mar',
+                  'Apr',
+                  'May',
+                  'Jun',
+                  'Jul',
+                  'Aug',
+                  'Sep',
+                  'Oct',
+                  'Nov',
+                  'Dec'
+                ],
+                today: 'Today',
+                clear: 'Clear'
+              }
             }
           }
         }
