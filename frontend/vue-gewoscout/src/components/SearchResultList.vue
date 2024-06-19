dbe51d467128bf5fd97aed9dbcb5a67253aa076c
julia <julia.sme27@gmail.com>
2024-06-19 04:48:28 +0200

    remove listings store and adding progress spinner

--- frontend/vue-gewoscout/src/components/SearchResultList.vue
+++ frontend/vue-gewoscout/src/components/SearchResultList.vue
@@ -2,12 +2,15 @@
 import Button from 'primevue/button';
 import Card from 'primevue/card';
 import ScrollTop from 'primevue/scrolltop';
+import ProgressSpinner from 'primevue/progressspinner';
 import { useListingsStore } from '@/common/store';
 
 const listingsStore = useListingsStore();
 
 function redirectToApartment(index: number) {
-  window.open(listingsStore.listings[index].detailsUrl, '_blank');
+  if (listingsStore.listings !== null) {
+    window.open(listingsStore.listings[index].detailsUrl, '_blank');
+  }
 }
 
 window.onscroll = () => {
@@ -23,45 +26,51 @@ window.onscroll = () => {
 </script>
 
 <template>
-  <h3 v-if="listingsStore.listings.length == 0" class="flex justify-content-center">
-    No apartments found, Sorry :(
-  </h3>
-  <div v-else class="cards mt-3 grid">
-    <div class="col-12 lg:col-4" v-for="(item, index) in listingsStore.listings" :key="index">
-      <Card style="overflow: hidden">
-        <template #header>
-          <img :src="item.previewImageUrl" width="420" height="180" />
-        </template>
-        <template #title>{{ item.title }}</template>
-        <template #subtitle
-          ><span class="pi pi-map-marker"></span> {{ item.postalCode }} {{ item.city }}</template
-        >
-        <template #content>
-          <div class="card flex justify-content-around">
-            <div class="flex flex-column m-0">
-              <p>Rooms</p>
-              <p class="text-center m-0">2</p>
+  <div class="flex justify-content-center">
+    <ProgressSpinner v-show="listingsStore.listings == null" />
+  </div>
+
+  <div v-if="listingsStore.listings !== null">
+    <h3 v-if="listingsStore.listings.length == 0" class="flex justify-content-center">
+      No apartments found, Sorry :(
+    </h3>
+    <div v-else class="cards mt-3 grid">
+      <div class="col-12 lg:col-4" v-for="(item, index) in listingsStore.listings" :key="index">
+        <Card style="overflow: hidden">
+          <template #header>
+            <img :src="item.previewImageUrl" width="420" height="180" />
+          </template>
+          <template #title>{{ item.title }}</template>
+          <template #subtitle
+            ><span class="pi pi-map-marker"></span> {{ item.postalCode }} {{ item.city }}</template
+          >
+          <template #content>
+            <div class="card flex justify-content-around">
+              <div class="flex flex-column m-0">
+                <p>Rooms</p>
+                <p class="text-center m-0">2</p>
+              </div>
+              <div class="flex flex-column m-0">
+                <p>Area</p>
+                <p class="text-center m-0">75 m²</p>
+              </div>
             </div>
-            <div class="flex flex-column m-0">
-              <p>Area</p>
-              <p class="text-center m-0">75 m²</p>
+          </template>
+          <template #footer>
+            <div class="flex gap-3 mt-1">
+              <Button label="Details" severity="secondary" outlined class="w-full" />
+              <Button
+                label="Request"
+                icon="pi pi-external-link"
+                class="w-full"
+                @click="redirectToApartment(index)"
+              />
             </div>
-          </div>
-        </template>
-        <template #footer>
-          <div class="flex gap-3 mt-1">
-            <Button label="Details" severity="secondary" outlined class="w-full" />
-            <Button
-              label="Request"
-              icon="pi pi-external-link"
-              class="w-full"
-              @click="redirectToApartment(index)"
-            />
-          </div>
-        </template>
-      </Card>
+          </template>
+        </Card>
+      </div>
+      <ScrollTop />
     </div>
-    <ScrollTop />
   </div>
 </template>
 
