c38e935583199eb04509aae0796cdb75e1c7e8d0
julia <julia.sme27@gmail.com>
2024-06-19 03:35:25 +0200

    fix vitest with mocking function and setting locale configs

--- frontend/vue-gewoscout/src/components/__tests__/SearchForm.spec.ts
+++ frontend/vue-gewoscout/src/components/__tests__/SearchForm.spec.ts
@@ -13,6 +13,14 @@ import 'primeflex/primeflex.css';
 describe('SearchForm', () => {
   beforeEach(() => {
     setActivePinia(createPinia());
+
+    window.matchMedia =
+      window.matchMedia ||
+      function () {
+        return {
+          addEventListener: function () {}
+        };
+      };
   });
 
   it('renders properly', () => {
@@ -26,48 +34,7 @@ describe('SearchForm', () => {
             config: {
               ripple: true,
               locale: {
-                firstDayOfWeek: 0, // Setting Sunday as the first day of the week
-                dayNames: [
-                  'Sunday',
-                  'Monday',
-                  'Tuesday',
-                  'Wednesday',
-                  'Thursday',
-                  'Friday',
-                  'Saturday'
-                ],
-                dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-                dayNamesMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-                monthNames: [
-                  'January',
-                  'February',
-                  'March',
-                  'April',
-                  'May',
-                  'June',
-                  'July',
-                  'August',
-                  'September',
-                  'October',
-                  'November',
-                  'December'
-                ],
-                monthNamesShort: [
-                  'Jan',
-                  'Feb',
-                  'Mar',
-                  'Apr',
-                  'May',
-                  'Jun',
-                  'Jul',
-                  'Aug',
-                  'Sep',
-                  'Oct',
-                  'Nov',
-                  'Dec'
-                ],
-                today: 'Today',
-                clear: 'Clear'
+                firstDayOfWeek: 0 // Setting Sunday as the first day of the week
               }
             }
           }
