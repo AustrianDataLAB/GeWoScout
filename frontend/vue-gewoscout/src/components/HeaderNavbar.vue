33a13b921d09de81d79483ea9fe8f73b5ffaacaf
Michael Koeppl <michael@koeppl.dev>
2024-06-25 12:16:49 +0200

    fix: adapt name of BWS-Gruppe cooperative

--- frontend/vue-gewoscout/src/components/HeaderNavbar.vue
+++ frontend/vue-gewoscout/src/components/HeaderNavbar.vue
@@ -50,7 +50,7 @@ const cities = ref([
 ]);
 
 const genos = ref([
-  { name: 'bwsg', code: 'bwsg' },
+  { name: 'BWS-Gruppe', code: 'BWS-Gruppe' },
   { name: 'ÖVW', code: 'oevw' },
   { name: 'wbv-gpa', code: 'wbv-gpa' }
 ]);
